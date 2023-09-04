de93a0fea03ec9342175397da754c29c9f846a94
Rohith-Daruri <darurirohith98@gmail.com>
2023-09-04 11:34:09 +0530

    Second change

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+      RWOnly r = new RWOnly();
+
+//      'name' has private access in 'com.driver.RWOnly'
+      r.setName("Rohith");
+      System.out.println(r.getName());
+  }
 }
